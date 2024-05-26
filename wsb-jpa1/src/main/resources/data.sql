7bd014202f3ce362a55dc8589f873134ac636b7f
Damian Wasylyk <damian_wasylyk@op.pl>
2024-05-26 16:58:35 +0200

    add a few addresses using SQL

--- wsb-jpa1/src/main/resources/data.sql
+++ wsb-jpa1/src/main/resources/data.sql
@@ -1,2 +1,4 @@
-insert into address (id, address_line1, address_line2, city, postal_code)
-            values (1, 'xx', 'yy', 'city', '62-030');
+insert into address (id, address_line1, address_line2, city, postal_code) values (1, 'xx', 'yy', 'city', '62-030');
+INSERT INTO ADDRESS (ID, ADDRESS_LINE1, ADDRESS_LINE2, CITY, POSTAL_CODE) VALUES (2, 'Ul. Piekna', '1', 'Warszawa', '01-001');
+INSERT INTO ADDRESS (ID, ADDRESS_LINE1, ADDRESS_LINE2, CITY, POSTAL_CODE) VALUES (2, 'Ul. Morska', '22', 'Warszawa', '01-002');
+INSERT INTO ADDRESS (ID, ADDRESS_LINE1, ADDRESS_LINE2, CITY, POSTAL_CODE) VALUES (2, 'Ul. Dzika', '333', 'Warszawa', '01-003');
